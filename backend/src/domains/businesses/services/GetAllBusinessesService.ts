41b4f60d278a9bfcc78d57b76810abcfe7d135a9
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-12-02 19:11:13 -0300

    Businesses: Add service to get all businesses from external source

---
+++ backend/src/domains/businesses/services/GetAllBusinessesService.ts
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+import AppError from '@shared/errors/AppError'
+import Business from '@domains/businesses/models/Business'
+
+const BUSINESSES_API_ENDPOINT = 'https://data.lacity.org/resource/6rrh-rzua.json'
+
+const LIMIT_PER_PAGE = 20
+
+class GetAllBusinessesService {
+  async run (page: number) {
+    const offset = (page - 1) * LIMIT_PER_PAGE
+
+    try {
+      const businessesResponse = await axios.get<Business[]>(
+        `${BUSINESSES_API_ENDPOINT}?$limit=${LIMIT_PER_PAGE}&$offset=${offset}`
+      )
+
+      return businessesResponse.data
+    } catch (error) {
+      throw new AppError(
+        'Error while fetching businesses. Try again, please',
+        500
+      )
+    }
+  }
+}
+
+export default GetAllBusinessesService
